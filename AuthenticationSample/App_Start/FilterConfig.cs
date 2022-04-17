cd8411466394ad26a2b7a0053e0c1135a7a8d29b
mhmd16-ha <https://github.com/mhmd16-ha/Todo_List.git>
2022-04-17 15:16:18 +0200

    Add project files.

---
+++ AuthenticationSample/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AuthenticationSample
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
